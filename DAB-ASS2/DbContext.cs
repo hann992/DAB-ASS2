e7038aab17d3c86d8f19a75c867d4a5bd7245e91
Andreas Steen Simmelkiær Sørensen <au196631@uni.au.dk>
2022-04-05 20:46:32 +0200

    dbcontext

---
+++ DAB-ASS2/DbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAB_ASS2
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=127.0.0.1,1433;Database=Municipality1;User ID=SA;Password=ZyurnYNmhXBP37Jdpr7a;"
+                );
+
+        }
+
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Society> Societies { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+
+    }
+}
+
+
+
+
+
+
