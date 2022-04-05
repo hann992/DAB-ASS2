eca66481398579b71f2f2d52016ea423bc9c84ed
patrick8975 <64925851+patrick8975@users.noreply.github.com>
2022-04-05 20:42:28 +0200

    Klasser lavet
    Klasser lavet

---
+++ DAB-ASS2/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAB_ASS2.Models
+{
+    public class Room
+    {
+        [Key]
+        public int room_ID { get; set; }
+        public string room_name { get; set; }
+        public int room_capacity { get; set; }
+        public ICollection<RoomProperty> Roomproperties { get; set; }
+    }
+}
---
+++ DAB-ASS2/Models/RoomProperty.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAB_ASS2.Models
+{
+    public class RoomProperty
+    {
+        [Key]
+        public int roomproperties_ID { get; set; }
+        public string room_property_name { get; set; }
+        public ICollection<Room> Rooms { get; set; }
+    }
+}
