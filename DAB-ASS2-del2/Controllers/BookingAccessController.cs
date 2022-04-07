5c52572981e962b610cc406d2958e428c04756b5
Andreas Steen Simmelkiær Sørensen <au196631@uni.au.dk>
2022-04-07 12:48:28 +0200

    så er der tekst

--- DAB-ASS2-del2/Controllers/BookingAccessController.cs
+++ DAB-ASS2-del2/Controllers/BookingAccessController.cs
@@ -16,7 +16,7 @@ namespace DAB_ASS2.Controllers
         [HttpGet("{keyResponsible}")]   // Vi tager en long int ind som key
         public string Get(long keyResponsible)
         {
-            Console.WriteLine(keyResponsible);
+            Console.WriteLine("ID: " + keyResponsible);
 
             /*
             Given a key-responsible the municipalities would like to offer a 
